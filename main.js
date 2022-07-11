ea400cfc166cd9bf9e02766d261945138117f001
orlando.tyrell <orlandotyrell@gmail.com>
2022-07-10 22:50:39 -0400

    Add main.js

---
+++ main.js
@@ -0,0 +1,10 @@
+
+let array = [13, 10, 18, 25, 30, 3];
+function twoSmallest (smallestNumber, secondSmallestNumber){
+    if (array > 2){
+        console.log["undefined"];
+    }else (smallestNumber, secondSmallestNumber);
+    }
+   console.log(twoSmallest[array]);
+
+
